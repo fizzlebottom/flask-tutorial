c9fbb902155f501fccacdb5d0c88b0e933e60abf
Marshall Harris <h0tpants83@gmail.com>
2019-05-21 15:03:22 -0700

    Add some helpful comments.

--- flaskr/db.py
+++ flaskr/db.py
@@ -6,6 +6,9 @@ from flask.cli import with_appcontext
 
 # Return db connection
 def get_db():
+    """ Connect to the application's configured database. The connection
+    is unique for each request and will be reused if this is called again.
+    """
     if 'db' not in g:
         g.db = sqlite3.connect(
             current_app.config['DATABASE'],
@@ -17,6 +20,9 @@ def get_db():
 
 # Close db connection
 def close_db(e=None):
+    """If this request connected to the database, close the
+    connection.
+    """
     db = g.pop('db', None)
 
     if db is not None:
