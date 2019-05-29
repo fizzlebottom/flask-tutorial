962db7e1dc3a51df8af51f047bb0dbd2378e0375
Marshall Harris <h0tpants83@gmail.com>
2019-05-29 13:53:41 -0700

    Add code to create blog post.

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -19,3 +19,30 @@ def index():
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required # User must be logged in to see the "Create" view
+def create():
+    """ Either the form is displayed or the posted data is validated and the post added to the db, or an error is shown.
+    """
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title: # If no title, set relevant error message
+            error = 'Title is required'
+
+        if error is not None:
+            flash(error) # Display the error message
+        else:
+            db = get_db() # Grab database
+            db.execute(
+                'INSERT INTO post (title, body, author_id)'
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['id'])
+            )
+            db.commit() # Insert the new post data into db
+            return redirect(url_for('blog.index')) # Go back to blog index
+
+    return render_template('blog/create.html')
